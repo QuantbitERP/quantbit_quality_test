79634a836214bb12d831c1257eef4e83b7cc079a
anushreelimaaye <anushree.limaye@erpdata.in>
2025-01-15 15:15:55 +0530

    New Doctypes created for Lab Testing

---
+++ quantbit_quality_test/quantbit_quality_test/doctype/mtc_generation_part___b/mtc_generation_part___b.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Quantbit Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("MTC Generation Part - B", {
+// 	refresh(frm) {
+
+// 	},
+// });
