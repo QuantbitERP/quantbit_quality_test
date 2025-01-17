b5451bd33990372f9d3989a8553db163a8e02ef9
anushreelimaaye <anushree.limaye@erpdata.in>
2025-01-17 17:52:00 +0530

    Added Workspace

--- quantbit_quality_test/hooks.py
+++ quantbit_quality_test/hooks.py
@@ -227,3 +227,12 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+   
+    {
+        "doctype": "Workspace",
+        "filters": [
+            ["name", "in", ["Quality and Lab Testing"]]
+        ],
+    },
+]
